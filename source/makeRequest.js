948706ef3afce0d83cef8f11edad54aa10d054f0
simon t <satabligan@ucdavis.edu>
2019-04-18 21:42:49 -0700

    Added time and temperature - top

--- source/makeRequest.js
+++ source/makeRequest.js
@@ -9,7 +9,100 @@ function createCORSRequest(method, url) {
   return xhr;
 }
 
+function loadMainIcon(weather, time) {
+  let main_img1 = document.getElementById("main-img1");
+  let main_img2 = document.getElementById("main-img2");
 
+  switch (weather) {
+    case "scattered clouds":
+      main_img1.src = "../assets/scatteredclouds.svg";
+      main_img2.src = "../assets/scatteredclouds.svg";
+      break;
+
+    case "broken clouds":
+      main_img1.src = "../assets/brokencloud.svg";
+      main_img2.src = "../assets/brokencloud.svg";
+      break;
+
+    case "few clouds":
+      if (time.getHours() < 6 || time.getHours > 18) {
+        main_img1.src = "../assets/fewclouds-night.svg";
+        main_img2.src = "../assets/fewclouds-day.svg";
+      } else {
+        main_img1.src = "../assets/clearsky.svg";
+        main_img2.src = "../assets/clearsky.svg";
+      }
+      break;
+
+    case "mist":
+      main_img1.src = "../assets/mist.svg";
+      main_img2.src = "../assets/mist.svg";
+      break;
+
+    case "clear sky":
+      if (time.getHours() < 6 || time.getHours > 18) {
+        main_img1.src = "../assets/clear-night.svg";
+        main_img2.src = "../assets/clear-night.svg";
+      } else {
+        main_img1.src = "../assets/clearsky.svg";
+        main_img2.src = "../assets/clearsky.svg";
+      }
+      break;
+
+    case "shower rain":
+      main_img1.src = "../assets/showerrain.svg";
+      main_img2.src = "../assets/showerrain.svg";
+      break;
+
+    case "thunderstorm":
+      main_img1.src = "../assets/showerrain.svg";
+      main_img2.src = "../assets/showerrain.svg";
+      break;
+
+    case "snow":
+      main_img1.src = "../assets/snow.svg";
+      main_img2.src = "../assets/snow.svg";
+      break;
+
+    default:
+      if (time.getHours() < 6 || time.getHours > 18) {
+        main_img1.src = "../assets/rain-night.svg";
+        main_img2.src = "../assets/rain-night.svg";
+      } else {
+        main_img1.src = "../assets/rain-day.svg";
+        main_img2.src = "../assets/rain-day.svg";
+      }
+      break;
+  }
+}
+
+function loadTime(time) {
+  let hour = time.getHours();
+  let ampm = "AM";
+
+  // adjust if necessary
+  if (time.getHours() >= 12) {
+    hour -= 12;
+    ampm = "PM";
+  }
+  timeOfDay = hour + ampm;
+
+  let time_web = document.getElementById("time-web");
+  let time_mobile = document.getElementById("time-mobile");
+
+  time_web.innerHTML = timeOfDay;
+  time_mobile.innerHTML = timeOfDay;
+}
+
+function loadTemperature(temperature) {
+  let temp = Math.trunc(temperature);
+
+  let temperature_web = document.getElementById("temperature-web");
+  let temperature_mobile = document.getElementById("temperature-mobile");
+
+  temperature_web.innerHTML = temp + "°";
+  temperature_mobile.innerHTML = temp + "°";
+}
 
 // Make the actual CORS request.
 function makeCorsRequest() {
@@ -31,70 +124,10 @@ function makeCorsRequest() {
       //console.log(object.list[0].weather[0].main);
       weather = object.list[16].weather[0].description;
       time = new Date(object.list[16].dt_txt);
-      let main_img1 = document.getElementById("main-img1");
-      let main_img2 = document.getElementById("main-img2");
-
-      switch (weather) {
-        case "scattered clouds":
-          main_img1.src = "../assets/scatteredclouds.svg";
-          main_img2.src = "../assets/scatteredclouds.svg";
-          break;
-
-        case "broken clouds":
-          main_img1.src = "../assets/brokencloud.svg";
-          main_img2.src = "../assets/brokencloud.svg";
-          break;
-
-        case "few clouds":
-          if (time.getHours() < 6 || time.getHours > 18) {
-            main_img1.src = "../assets/fewclouds-night.svg";
-            main_img2.src = "../assets/fewclouds-day.svg";
-          } else {
-            main_img1.src = "../assets/clearsky.svg";
-            main_img2.src = "../assets/clearsky.svg";
-          }
-          break;
-
-        case "mist":
-          main_img1.src = "../assets/mist.svg";
-          main_img2.src = "../assets/mist.svg";
-          break;
-
-        case "clear sky":
-          if (time.getHours() < 6 || time.getHours > 18) {
-            main_img1.src = "../assets/clear-night.svg";
-            main_img2.src = "../assets/clear-night.svg";
-          } else {
-            main_img1.src = "../assets/clearsky.svg";
-            main_img2.src = "../assets/clearsky.svg";
-          }
-          break;
-
-        case "shower rain":
-          main_img1.src = "../assets/showerrain.svg";
-          main_img2.src = "../assets/showerrain.svg";
-          break;
-
-        case "thunderstorm":
-          main_img1.src = "../assets/showerrain.svg";
-          main_img2.src = "../assets/showerrain.svg";
-          break;
-
-        case "snow":
-          main_img1.src = "../assets/snow.svg";
-          main_img2.src = "../assets/snow.svg";
-          break;
-
-        default:
-          if (time.getHours() < 6 || time.getHours > 18) {
-            main_img1.src = "../assets/rain-night.svg";
-            main_img2.src = "../assets/rain-night.svg";
-          } else {
-            main_img1.src = "../assets/rain-day.svg";
-            main_img2.src = "../assets/rain-day.svg";
-          }
-          break;
-      }
+      temperature = object.list[16].main.temp;
+      loadMainIcon(weather, time);
+      loadTime(time);
+      loadTemperature(temperature);
   };
 
   xhr.onerror = function() {
