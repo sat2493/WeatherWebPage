11fd65e1ea376d24cbbaaa302b5d5c8ce20003bd
simon t <satabligan@ucdavis.edu>
2019-04-20 16:35:27 -0700

    added Future Weather stats

--- source/makeRequest.js
+++ source/makeRequest.js
@@ -104,6 +104,90 @@ function loadTemperature(temperature) {
   temperature_mobile.innerHTML = temp + "°";
 }
 
+function loadFutureIcons (futureWeather, time, i) {
+  let id = "main-img" + i;
+  console.log(futureWeather);
+  console.log(id);
+  let main_img = document.getElementById(id);
+
+  switch (futureWeather) {
+    case "scattered clouds":
+      main_img.src = "../assets/scatteredclouds.svg";
+      break;
+
+    case "broken clouds":
+      main_img.src = "../assets/brokencloud.svg";
+      break;
+
+    case "few clouds":
+      if (time.getHours() < 6 || time.getHours > 18) {
+        main_img.src = "../assets/fewclouds-night.svg";
+      } else {
+        main_img.src = "../assets/clearsky.svg";
+      }
+      break;
+
+    case "mist":
+      main_img.src = "../assets/mist.svg";
+      break;
+
+    case "clear sky":
+      if (time.getHours() < 6 || time.getHours > 18) {
+        main_img.src = "../assets/clear-night.svg";
+      } else {
+        main_img.src = "../assets/clearsky.svg";
+      }
+      break;
+
+    case "shower rain":
+      main_img.src = "../assets/showerrain.svg";
+      break;
+
+    case "thunderstorm":
+      main_img.src = "../assets/showerrain.svg";
+      break;
+
+    case "snow":
+      main_img.src = "../assets/snow.svg";
+      break;
+
+    default:
+      if (time.getHours() < 6 || time.getHours > 18) {
+        main_img.src = "../assets/rain-night.svg";
+      } else {
+        main_img.src = "../assets/rain-day.svg";
+      }
+      break;
+  }
+}
+
+function loadFutureTime(futureTime, i) {
+  let hour = futureTime.getHours();
+  let ampm = "AM";
+
+  // adjust if necessary
+  if (futureTime.getHours() >= 12) {
+    hour -= 12;
+    ampm = "PM";
+  }
+
+  timeOfDay = hour + ":00 " + ampm;
+  id = "time" + i;
+
+  let time = document.getElementById(id);
+
+  time.innerHTML = timeOfDay;
+}
+
+function loadFutureTemperature(futureTemperature, i) {
+  let temp = Math.trunc(futureTemperature);
+  let id = "temperature" + i;
+
+  let temperature = document.getElementById(id);
+
+  temperature.innerHTML = temp + "°";
+}
+
 // Make the actual CORS request.
 function makeCorsRequest() {
 
@@ -121,13 +205,25 @@ function makeCorsRequest() {
   xhr.onload = function() {
       let responseStr = xhr.responseText;  // get the JSON string
       let object = JSON.parse(responseStr);  // turn it into an object
-      //console.log(object.list[0].weather[0].main);
-      weather = object.list[16].weather[0].description;
-      time = new Date(object.list[16].dt_txt);
-      temperature = object.list[16].main.temp;
+      console.log(object.list[0].weather[0].main);
+      let weather = object.list[16].weather[0].description;
+      let time = new Date(object.list[16].dt_txt);
+      let temperature = object.list[16].main.temp;
+
       loadMainIcon(weather, time);
       loadTime(time);
       loadTemperature(temperature);
+
+      let futureDays = 5;
+      for (i = 1, count = 0; count < futureDays; i++, count++) {
+        let futureWeather = object.list[16 + i].weather[0].description;
+        let futureTime = new Date(object.list[16 + i].dt_txt);
+        let futureTemperature = object.list[16 + i].main.temp;
+
+        loadFutureIcons(futureWeather, time, i + 2);
+        loadFutureTime(futureTime, i);
+        loadFutureTemperature(futureTemperature, i);
+      }
   };
 
   xhr.onerror = function() {
