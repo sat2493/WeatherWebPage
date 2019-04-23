1349978c3053a8272b98c58ef0a8e15693631c9f
simon t <satabligan@ucdavis.edu>
2019-04-23 02:47:53 -0700

    Testing animations

---
+++ Move Up and Down/UpjQuery.js
@@ -0,0 +1,33 @@
+var mintop = 5;
+var maxtop = 1000;
+var time = 1000;
+var timer = null;
+var toggled = false;
+
+// from: https://stackoverflow.com/questions/3795481/javascript-slidedown-without-jquery 
+window.onload = function() {
+  var controller = document.getElementById('toggleThis');
+  var content = document.getElementById('content')
+  controller.onclick = function() {
+    clearInterval(timer);
+    var instancetop = parseInt(content.style.top);
+    var init = (new Date()).getTime();
+    var top = (toggled = !toggled) ? maxtop : mintop;
+
+    var disp = top - parseInt(content.style.top);
+
+
+    timer = setInterval(function() {
+      var instance = (new Date()).getTime() - init;
+      if (instance <= time) {
+        var pos = instancetop + Math.floor(disp * instance / time);;
+        content.style.top = pos + "px";
+      } else {
+        content.style.top = top + "px";
+        clearInterval(timer);
+      }
+    }, 1);
+
+
+  };
+};
